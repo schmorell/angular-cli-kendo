b3347b3da7e3fc9abb9e0a819848103179bdeeec
Felix Schmorell <felix.schmorell@dialog.ch>
2017-07-03 11:25:01 +0200

    kendo integration

--- src/app/ng1-app.module.ts
+++ src/app/ng1-app.module.ts
@@ -1,9 +1,10 @@
 import * as angular from 'angular'
 import uiRouter from 'angular-ui-router';
+import '@progress/kendo-ui';
 
 import { RootModule } from './root/root.module';
 
-export const Ng1AppModule = angular.module('Ng1AppModule', [uiRouter, RootModule.name]);
+export const Ng1AppModule = angular.module('Ng1AppModule', [uiRouter, RootModule.name, 'kendo.directives']);
 
 Ng1AppModule.config(($locationProvider) => {
   $locationProvider.html5Mode(true)
--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,5 +1,6 @@
 export const RootComponent = {
     template: `
         <h1>Root Component</h1>
+        <div kendo-color-palette></div>
     `
 };
